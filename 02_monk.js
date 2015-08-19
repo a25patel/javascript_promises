5c303fe20e3e71b039a2cfcca2c81c40c46b1270
a25patel <aanaletap2510@gmail.com>
2015-08-19 14:47:12 -0600

    02monk refactored to promises

--- 02_monk.js
+++ 02_monk.js
@@ -1,64 +1,39 @@
 var db = require('monk')('localhost/javascript-promises-examples')
 var users = db.get('users')
 
+// PROMISES
 
-var fraggle = users.find({})
-
-
-
-
-users.remove({})
-  .then(function () {
-  users.insert({name: 'Joe'}, function (err, result) {
-    users.insert({name: 'Sue'}, function (err, result) {
-      users.insert({name: 'Tim'}, function (err, result) {
-        users.insert({name: 'Kim'}, function (err, result) {
-          users.find({}, function (err, results) {
-            console.log("\nSuccess! The records are: \n");
-            console.log(results);
-            db.close()
-          })
-        })
-      })
-    })
-  })
-})
-
-
-
-
-var calls = {
-  remove: function(){
-    return users.remove({}).then(function(result){
-      var master = {}
-      master.result = result;
-      return master
-    })
-  },
-
-  insert1: function(name){
-    return users.insert({name: 'Joe'}).then(function(result){
-      master.results = result
-      return master
-    })
-  },
-}
+users.remove({}).then(function(){
+  return users.insert({name: 'Joe'})
+}).then(function(){
+  return users.insert({name: 'Sue'})
+}).then(function(){
+  return users.insert({name: 'Tim'})
+}).then(function(){
+  return users.insert({name: 'Kim'})
+}).then(function(){
+  return users.find({})
+}).then(function(results){
+  console.log("\nSuccess! The records are: \n");
+  console.log(results);
+  db.close()
+});
 
 
 // CallBack
-
-users.remove({}, function (err) {
-  users.insert({name: 'Joe'}, function (err, result) {
-    users.insert({name: 'Sue'}, function (err, result) {
-      users.insert({name: 'Tim'}, function (err, result) {
-        users.insert({name: 'Kim'}, function (err, result) {
-          users.find({}, function (err, results) {
-            console.log("\nSuccess! The records are: \n");
-            console.log(results);
-            db.close()
-          })
-        })
-      })
-    })
-  })
-})
+//
+// users.remove({}, function (err) {
+//   users.insert({name: 'Joe'}, function (err, result) {
+//     users.insert({name: 'Sue'}, function (err, result) {
+//       users.insert({name: 'Tim'}, function (err, result) {
+//         users.insert({name: 'Kim'}, function (err, result) {
+//           users.find({}, function (err, results) {
+//             console.log("\nSuccess! The records are: \n");
+//             console.log(results);
+//             db.close()
+//           })
+//         })
+//       })
+//     })
+//   })
+// })
