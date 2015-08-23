dbcb9ff74f95309bb53d4816c93dca7e0f1a094c
a25patel <aanaletap2510@gmail.com>
2015-08-22 18:24:17 -0600

    working on it

--- challenges/02.js
+++ challenges/02.js
@@ -2,6 +2,17 @@ var db = require('monk')('localhost/stocks-example')
 var Post = db.get('posts')
 var Comment = db.get('comments')
 
+
+Post.remove({}).then(function(){
+  Post.insert({newPost: 'Hello'})
+  return {newPost: newPost};
+}).then(function(obj){
+  console.log('success');
+  console.log(obj);
+  db.close();
+})
+
+
 // delete all posts and
 // then insert a post and
 // then insert another post and
