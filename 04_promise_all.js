8f3f1660c13c5796bbad73782941e9cb08bfcbd5
a25patel <aanaletap2510@gmail.com>
2015-08-25 16:26:42 -0600

    promise.all understood

--- 04_promise_all.js
+++ 04_promise_all.js
@@ -3,27 +3,41 @@ var db = require('monk')('localhost/javascript-promises-examples')
 var users = db.get('users')
 var promise = users.insert
 
+
 users.remove({}).then(function(){
-  Promise.all([
+  return Promise.all([
     users.insert({name: 'Joe'}),
     users.insert({name: 'Sue'}),
     users.insert({name: 'Tim'}),
-    users.insert({name: 'Kim'}),
-  ])
-.then(function(result){
-    users.insert({name: 'Sue'}, function (err, result) {
-      users.insert({name: 'Tim'}, function (err, result) {
-        users.insert({name: 'Kim'}, function (err, result) {
-          users.find({}, function (err, results) {
-            console.log("\nSuccess! The records are: \n");
-            console.log(results);
-            db.close()
-          })
-        })
-      })
-    })
-  })
-})
+    users.insert({name: 'Kim'})
+  ]).then(function(result){
+    return users.find({})
+  }).then(function(result){
+    console.log('\nSuccess! The records are: \n');
+    console.log(result);
+    db.close()
+  });
+
+});
+
+
+
+
+// users.remove({}).then(function(){
+// .then(function(result){
+//     users.insert({name: 'Sue'}, function (err, result) {
+//       users.insert({name: 'Tim'}, function (err, result) {
+//         users.insert({name: 'Kim'}, function (err, result) {
+//           users.find({}, function (err, results) {
+//             console.log("\nSuccess! The records are: \n");
+//             console.log(results);
+//             db.close()
+//           })
+//         })
+//       })
+//     })
+//   })
+// })
 
 
 
