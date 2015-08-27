755c0ce46bfb0f27a36c4dd0cff919b29dc20e0a
a25patel <aanaletap2510@gmail.com>
2015-08-27 11:16:59 -0600

    monk 02 complete refactored into promises

--- 02_monk.js
+++ 02_monk.js
@@ -1,30 +1,28 @@
 var db = require('monk')('localhost/javascript-promises-examples')
 var users = db.get('users')
 
-//redo the entire promises repo with all challenges complete! 
 
 
 // PROMISES
-
-users.remove({}, function (err) {
-  users.insert({name: 'Joe'}, function (err, result) {
-    users.insert({name: 'Sue'}, function (err, result) {
-      users.insert({name: 'Tim'}, function (err, result) {
-        users.insert({name: 'Kim'}, function (err, result) {
-          users.find({}, function (err, results) {
-            console.log("\nSuccess! The records are: \n");
-            console.log(results);
-            db.close()
-          })
-        })
-      })
-    })
-  })
+users.remove({}).then(function(){
+  return users.insert({name:'Joe'})
+}).then(function(result){
+  return users.insert({name:'Sue'})
+}).then(function(result){
+  return users.insert({name:'Tim'})
+}).then(function(result){
+  return users.insert({name:'Kim'})
+}).then(function(result){
+  return users.find({})
+}).then(function(result){
+  console.log('\nSuccess! The records are:\n');
+  console.log(result);
+  db.close()
 })
 
 
-// CallBack
-//
+// CAlLBACKS
+
 // users.remove({}, function (err) {
 //   users.insert({name: 'Joe'}, function (err, result) {
 //     users.insert({name: 'Sue'}, function (err, result) {
@@ -40,19 +38,3 @@ users.remove({}, function (err) {
 //     })
 //   })
 // })
-//
-// users.remove({}).then(function(){
-//   return users.insert({name: 'Joe'})
-// }).then(function(){
-//   return users.insert({name: 'Sue'})
-// }).then(function(){
-//   return users.insert({name: 'Tim'})
-// }).then(function(){
-//   return users.insert({name: 'Kim'})
-// }).then(function(){
-//   return users.find({})
-// }).then(function(results){
-//   console.log("\nSuccess! The records are: \n");
-//   console.log(results);
-//   db.close()
-// });
