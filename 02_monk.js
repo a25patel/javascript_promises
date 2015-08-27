e3864a50e49224797fd6150e459b1e1565032d1d
a25patel <aanaletap2510@gmail.com>
2015-08-27 11:03:50 -0600

    notes

--- 02_monk.js
+++ 02_monk.js
@@ -1,23 +1,26 @@
 var db = require('monk')('localhost/javascript-promises-examples')
 var users = db.get('users')
 
+//redo the entire promises repo with all challenges complete! 
+
+
 // PROMISES
 
-users.remove({}).then(function(){
-  return users.insert({name: 'Joe'})
-}).then(function(){
-  return users.insert({name: 'Sue'})
-}).then(function(){
-  return users.insert({name: 'Tim'})
-}).then(function(){
-  return users.insert({name: 'Kim'})
-}).then(function(){
-  return users.find({})
-}).then(function(results){
-  console.log("\nSuccess! The records are: \n");
-  console.log(results);
-  db.close()
-});
+users.remove({}, function (err) {
+  users.insert({name: 'Joe'}, function (err, result) {
+    users.insert({name: 'Sue'}, function (err, result) {
+      users.insert({name: 'Tim'}, function (err, result) {
+        users.insert({name: 'Kim'}, function (err, result) {
+          users.find({}, function (err, results) {
+            console.log("\nSuccess! The records are: \n");
+            console.log(results);
+            db.close()
+          })
+        })
+      })
+    })
+  })
+})
 
 
 // CallBack
@@ -37,3 +40,19 @@ users.remove({}).then(function(){
 //     })
 //   })
 // })
+//
+// users.remove({}).then(function(){
+//   return users.insert({name: 'Joe'})
+// }).then(function(){
+//   return users.insert({name: 'Sue'})
+// }).then(function(){
+//   return users.insert({name: 'Tim'})
+// }).then(function(){
+//   return users.insert({name: 'Kim'})
+// }).then(function(){
+//   return users.find({})
+// }).then(function(results){
+//   console.log("\nSuccess! The records are: \n");
+//   console.log(results);
+//   db.close()
+// });
