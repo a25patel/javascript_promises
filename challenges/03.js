1a84761a146f7e00343a4b0a1fd7f07ec540fcf8
a25patel <aanaletap2510@gmail.com>
2015-08-27 15:46:29 -0600

    challenge 3 complete

--- challenges/03.js
+++ challenges/03.js
@@ -2,6 +2,25 @@ var db = require('monk')('localhost/stocks-example')
 var Post = db.get('posts')
 var Comment = db.get('comments')
 
+Post.remove({}).then(function(){
+  return Promise.all([
+    Post.insert({task: 'Clean room'}),
+    Post.insert({task: 'Vaccuum'}),
+    Post.insert({task: 'Get Mail'}),
+    Post.insert({task: 'get groceries'})
+  ])
+}).then(function(result){
+  return Post.find({})
+}).then(function(result){
+  console.log(result);
+  console.log('Success!');
+  db.close();
+})
+
+
+
+
+
 // delete all posts and
 // then insert 4 different posts
 // then when all posts have been inserted
