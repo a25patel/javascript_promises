431d71af93a757d66721c2dfeb5f34ed213b2a90
a25patel <aanaletap2510@gmail.com>
2015-08-28 12:31:56 -0600

    Done with promise challenges! totally understand this now

--- challenges/06.js
+++ challenges/06.js
@@ -1,6 +1,29 @@
 var db = require('monk')('localhost/stocks-example')
-var Post = db.get('posts')
-var Comment = db.get('comments')
+var post = db.get('posts')
+var comment = db.get('comments')
+
+post.remove({}).then(function(){
+  return comment.remove({})
+}).then(function(){
+  return post.insert({post: 'Galvanize is the bees kness'}).then(function(result){
+    var master = {};
+    master.post = result
+    return master
+  })
+}).then(function(master){
+  return comment.insert({comment: 'Hell yes it is!', postId: master.post._id}).then(function(result){
+    master.comment = result;
+    return master
+  })
+}).then(function(master){
+  db.close();
+  console.log(master.post);
+  console.log(master.comment);
+})
+
+
+
+
 
 // delete all posts and all comments
 // and once they are deleted
