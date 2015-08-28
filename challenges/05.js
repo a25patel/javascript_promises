218a89f61386fb5975a29371c93ef6ab367c6a3f
a25patel <aanaletap2510@gmail.com>
2015-08-28 12:23:24 -0600

    FINALLY UNDERSTANDING PROMISES AND MONGO ASSOCIATIONS COMBINATIONS

--- challenges/05.js
+++ challenges/05.js
@@ -1,6 +1,31 @@
 var db = require('monk')('localhost/stocks-example')
-var Post = db.get('posts')
-var Comment = db.get('comments')
+var post = db.get('posts')
+var comment = db.get('comments')
+
+post.remove({}).then(function(){
+  return comment.remove({})
+}).then(function(){
+  return post.insert({post:"Omg! Can't wait for labor day weekend"}).then(function(result){
+    var masterObject = {};
+    masterObject.post = result
+    return masterObject
+  })
+}).then(function(masterObject){
+  return Promise.all([
+    comment.insert({comment: 'Omg neither can i!', postId: masterObject.post._id}),
+    comment.insert({comment: 'Yay lets do something!', postId: masterObject.post._id}),
+    comment.insert({comment: 'Mimosas and samosas forever!', postId: masterObject.post._id})
+  ]).then(function(results){
+    masterObject.comment = results;
+    return masterObject
+  })
+}).then(function(masterObject){
+  console.log('Success!');
+  console.log(masterObject);
+  db.close();
+})
+
+
 
 // delete all posts and all comments
 // and once they are deleted
