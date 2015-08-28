efb2edead7a4bf7a2b7bd7904ed93370afd577bc
a25patel <aanaletap2510@gmail.com>
2015-08-28 12:10:44 -0600

    got number 4!!!

--- challenges/04.js
+++ challenges/04.js
@@ -1,6 +1,29 @@
 var db = require('monk')('localhost/stocks-example')
-var Post = db.get('posts')
-var Comment = db.get('comments')
+var post = db.get('posts')
+var comment = db.get('comments')
+
+
+
+post.remove({}).then(function(){
+  return comment.remove({})
+}).then(function(){
+  return post.insert({task: 'Clean room'}).then(function(result){
+    var masterObject = {};
+    masterObject.post = result;
+    return masterObject;
+  })
+}).then(function(masterObject){
+  return comment.insert({comment: 'Food', postId: masterObject.post._id}).then(function(result){
+    masterObject.comment = result;
+    return masterObject;
+  })
+}).then(function(masterObject){
+  console.log('Success!');
+  console.log(masterObject);
+  db.close();
+})
+
+
 
 // delete all posts and all comments
 // and when all documents in both collections are empty
